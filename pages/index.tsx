51bb412001200782893eef3d5e7275655691be62
David Venegas <venegasdavidm@gmail.com>
2023-03-05 18:00:03 -0500

    icons and logos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import s from '@/styles/index.module.scss';
 import Head from 'next/head'
 import Image from 'next/image'
+import { MdOutlineSaveAlt, MdOutlineImageSearch } from 'react-icons/md'
+import { TbCloudUpload } from 'react-icons/tb'
+import { FaRegLaughBeam } from 'react-icons/fa'
 
 
 export default function Home() {
@@ -16,19 +19,19 @@ export default function Home() {
 
         <div className={s.banner}>
           <Image
-            className={s.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
+            className={s.logo_text}
+            src="/logo_text.svg"
+            alt="Meeme"
+            width={250}
+            height={80}
             priority
           />
-          <div className={s.thirteen}>
+          <div className={s.logo}>
             <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
+              src="/logo.svg"
+              alt="Meeme Logo"
+              width={55}
+              height={55}
               priority
             />
           </div>
@@ -37,23 +40,23 @@ export default function Home() {
         <div className={s.grid}>
 
           <a href="/" target="_blank" rel="noopener noreferrer" className={s.card}>
-            <h2>Save <span>-&gt;</span></h2>
-            <p>Easily upload and save your memes from anywhere.</p>
+            <h2>Save <MdOutlineSaveAlt className={s.icon_1}/></h2>
+            <p>Easily download and save your memes from anywhere.</p>
           </a>
 
           <a href="/" target="_blank" rel="noopener noreferrer" className={s.card}>
-            <h2>Store <span>-&gt;</span></h2>
+            <h2>Store <TbCloudUpload className={s.icon_2}/></h2>
             <p>Securely store your memes in the cloud for easy access.</p>
           </a>
 
           <a href="/" target="_blank" rel="noopener noreferrer" className={s.card}>
-            <h2>Search <span>-&gt;</span></h2>
-            <p>Instantly find that meme you just can&#39;t remember the name of.</p>
+            <h2>Search <MdOutlineImageSearch className={s.icon_3}/></h2>
+            <p>Instantly find that meme you can&#39;t remember the name of.</p>
           </a>
 
           <a href="/" target="_blank" rel="noopener noreferrer" className={s.card}>
-            <h2>Share <span>-&gt;</span></h2>
-            <p>Share your memes with friends and family with just a tap.</p>
+            <h2>Share <FaRegLaughBeam className={s.icon_4}/></h2>
+            <p>Send your memes to friends and family with just a tap.</p>
           </a>
 
         </div>
