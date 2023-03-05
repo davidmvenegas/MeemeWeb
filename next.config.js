106c339296ee0b268dc28c3a8bf152f2098f37fc
David Venegas <venegasdavidm@gmail.com>
2023-03-05 11:51:10 -0500

    setup of web app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  scssOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
